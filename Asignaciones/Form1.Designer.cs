d5361df405fa739157b232c0e9d94c571e41cc48
S. Ulises Ferreyra <ulises.ferreyra@hotmail.com>
2019-06-25 02:25:46 -0300

    Nuevo mejorado

--- Asignaciones/Form1.Designer.cs
+++ Asignaciones/Form1.Designer.cs
@@ -28,13 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtFecAlta = new System.Windows.Forms.TextBox();
+            this.cmdAceptar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstProg = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmdAsignar = new System.Windows.Forms.Button();
+            this.lstDef = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(467, 227);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 0;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(24, 95);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(246, 20);
+            this.txtDescripcion.TabIndex = 1;
+            // 
+            // txtFecAlta
+            // 
+            this.txtFecAlta.Location = new System.Drawing.Point(467, 329);
+            this.txtFecAlta.Name = "txtFecAlta";
+            this.txtFecAlta.Size = new System.Drawing.Size(100, 20);
+            this.txtFecAlta.TabIndex = 2;
+            // 
+            // cmdAceptar
+            // 
+            this.cmdAceptar.Location = new System.Drawing.Point(467, 372);
+            this.cmdAceptar.Name = "cmdAceptar";
+            this.cmdAceptar.Size = new System.Drawing.Size(258, 23);
+            this.cmdAceptar.TabIndex = 3;
+            this.cmdAceptar.Text = "cmdAceptar";
+            this.cmdAceptar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtDescripcion);
+            this.groupBox1.Location = new System.Drawing.Point(443, 185);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(308, 229);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nuevo defecto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Fecha de Alta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Descripcion";
+            // 
+            // lstProg
+            // 
+            this.lstProg.FormattingEnabled = true;
+            this.lstProg.Location = new System.Drawing.Point(27, 19);
+            this.lstProg.Name = "lstProg";
+            this.lstProg.Size = new System.Drawing.Size(247, 160);
+            this.lstProg.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmdAsignar);
+            this.groupBox2.Controls.Add(this.lstProg);
+            this.groupBox2.Location = new System.Drawing.Point(37, 185);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(308, 229);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Asignar defecto a...";
+            // 
+            // cmdAsignar
+            // 
+            this.cmdAsignar.Location = new System.Drawing.Point(27, 187);
+            this.cmdAsignar.Name = "cmdAsignar";
+            this.cmdAsignar.Size = new System.Drawing.Size(247, 23);
+            this.cmdAsignar.TabIndex = 7;
+            this.cmdAsignar.Text = "cmdAsignar";
+            this.cmdAsignar.UseVisualStyleBackColor = true;
+            this.cmdAsignar.Click += new System.EventHandler(this.cmdAsignar_Click);
+            // 
+            // lstDef
+            // 
+            this.lstDef.FormattingEnabled = true;
+            this.lstDef.Location = new System.Drawing.Point(22, 38);
+            this.lstDef.Name = "lstDef";
+            this.lstDef.Size = new System.Drawing.Size(747, 134);
+            this.lstDef.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Defectos sin resolver";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstDef);
+            this.Controls.Add(this.cmdAceptar);
+            this.Controls.Add(this.txtFecAlta);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "AsigForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtFecAlta;
+        private System.Windows.Forms.Button cmdAceptar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstProg;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button cmdAsignar;
+        private System.Windows.Forms.ListBox lstDef;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Asignaciones/Form1.cs
+++ Asignaciones/Form1.cs
@@ -16,5 +16,62 @@ namespace Asignaciones
         {
             InitializeComponent();
         }
+        string programador_seleccionado = "";
+        List<Defectos> Lista_defectos = new List<Defectos>();
+
+        /// funcion para validar fecha
+        private bool validarFecha(string f)
+        {
+            DateTime fecha = new DateTime();
+            DateTime fechaHoy = DateTime.Now;
+            TimeSpan diferencia = new TimeSpan();
+
+            if (DateTime.TryParse(f, out fecha))
+            {
+                diferencia = fecha - fechaHoy;
+
+                if (diferencia.Days >= 0 || diferencia.Hours >= 0)
+                {
+                    MessageBox.Show(diferencia.ToString());
+                    return true;
+                }
+            }
+
+            return false;
+
+
+          
+        }
+
+
+
+
+       
+
+        private void cmdAsignar_Click(object sender, EventArgs e)
+        {
+            {
+                if (lstProg.SelectedIndex < 0)
+                {
+                    MessageBox.Show("No selecciono ningun programador");
+                }
+                else
+                {
+                    programador_seleccionado = lstProg.SelectedItem.ToString();
+                    lstProg.SelectedIndex = -1;
+                }
+            }
+
+           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstProg.Items.Add("Juan Perez");
+            lstProg.Items.Add("Lorena Lopez");
+            lstProg.Items.Add("Juana Cramer");
+            lstProg.Items.Add("Felipe Cuzzella");
+        }
     }
 }
